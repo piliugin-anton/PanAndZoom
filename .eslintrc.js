0689486855f9f64e3052ce884fe7b2b7f136a690
Anton Piliugin <anton.piliugin@icloud.com>
2022-09-05 03:32:30 +0500

    Packages updated, removed unused deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
-  extends: '@Alhadis',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  rules: {
-    "consistent-this": 0,
-    "prefer-rest-params": 0
-  },
   overrides: [
     {
       files: [
